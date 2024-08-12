99cc8419a30e02d0968710afb4ce4b0e9c4ca2f9
shubham70602 <144945500+shubham70602@users.noreply.github.com>
2024-08-12 15:57:24 +0000

    container config data

---
+++ basic/datatype.js
@@ -0,0 +1,4 @@
+let name = "Shubham"
+let age = 22
+let isloggedIn = false
+console.log(typeof(null))
---
+++ basic/variable.js
@@ -0,0 +1,11 @@
+const accountId = 152154
+let accountEmail = "shubham@google.com"
+var accountPassword = "12111115"
+accountCity = " Amroha"
+accountEmail = "shubh@gmail"
+accountPassword = "3556"
+accountCity = "DELHI"
+
+// accountId = 15
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
